7799270091aa562f71ff6e9f3343d91fad4d391b
Herbert Poul <herbert@codeux.design>
2020-01-17 16:21:25 +0100

    updated fastlane.

--- authpass/android/app/src/main/java/design/codeux/authpass/MainActivity.java
+++ authpass/android/app/src/main/java/design/codeux/authpass/MainActivity.java
@@ -3,30 +3,37 @@ package design.codeux.authpass;
 import android.os.Bundle;
 import android.provider.Settings;
 
-import androidx.annotation.NonNull;
-import io.flutter.app.FlutterActivity;
-import io.flutter.app.FlutterFragmentActivity;
-import io.flutter.plugin.common.MethodCall;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.android.SplashScreen;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterFragmentActivity {
-  private static final String CHANNEL = "app.authpass/misc";
+    private static final String CHANNEL = "app.authpass/misc";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
-      @Override
-      public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        if ("isFirebaseTestLab".equals(call.method)) {
-          result.success("true".equals(Settings.System.getString(getContentResolver(), "firebase.test.lab")));
-          return;
-        }
-        result.notImplemented();
-      }
-    });
-  }
+    }
+
+    @Override
+    public SplashScreen provideSplashScreen() {
+        return null;
+    }
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler((call, result) -> {
+            if ("isFirebaseTestLab".equals(call.method)) {
+                result.success("true".equals(Settings.System.getString(getContentResolver(), "firebase.test.lab")));
+                return;
+            }
+            result.notImplemented();
+        });
+    }
 }
