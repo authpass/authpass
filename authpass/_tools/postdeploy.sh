31f86ff3932e2461f4a5ea180734a58d0aa94f91
Herbert Poul <herbert@codeux.design>
2020-02-05 11:36:23 +0100

    don't write blackbox secret into temporary file.

--- authpass/_tools/postdeploy.sh
+++ authpass/_tools/postdeploy.sh
@@ -6,19 +6,16 @@ BLACKBOX_SECRET=${BLACKBOX_SECRET:-}
 
 if test -z "${BLACKBOX_SECRET}" ; then
     echo "Require setting of BLACKBOX_SECRET"
-fi
-
-secret=.tmp/BLACKBOX_SECRET
-mkdir .tmp
-
-echo "${BLACKBOX_SECRET}" > .tmp/BLACKBOX_SECRET
-
-if ! test -e "$secret" ; then
-    echo "Require $secret file to contain blackbox private key."
     exit 1
 fi
 
-authpass/_tools/blackbox.go.linux cipostdeploy "$secret"
+tmpdir=$(mktemp -d)
+blackbox='blackbox.go.linux.amd64'
 
-rm "$secret"
+curl -L -o ${tmpdir}/${blackbox} https://github.com/hpoul/blackbox/releases/download/golang-v0.1-cipostdeploy/blackbox.go.linux.amd64
 
+chmod +x ${tmpdir}/${blackbox}
+
+echo "${BLACKBOX_SECRET}" | ${tmpdir}/${blackbox} cipostdeploy -
+
+rm -rf ${tmpdir}
