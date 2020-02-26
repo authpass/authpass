faab1ac54a2bf9b32e3a124efe4bccff62d7a210
Herbert Poul <herbert@codeux.design>
2020-02-26 17:31:18 +0100

    move base functionality of argon2 into kdbx library

--- authpass/lib/bloc/kdbx_argon2_ffi.dart
+++ authpass/lib/bloc/kdbx_argon2_ffi.dart
@@ -33,54 +33,15 @@ typedef Argon2Hash = Pointer<Utf8> Function(
   int version,
 );
 
-class FlutterArgon2 implements Argon2 {
+class FlutterArgon2 extends Argon2Base {
   FlutterArgon2() {
     final argon2lib = Platform.isAndroid
         ? DynamicLibrary.open('libargon2_ffi.so')
         : DynamicLibrary.executable();
-    _argon2hash = argon2lib
+    argon2hash = argon2lib
         .lookup<NativeFunction<Argon2HashNative>>('hp_argon2_hash')
         .asFunction();
   }
-  Argon2Hash _argon2hash;
-
   @override
-  Uint8List argon2(
-    Uint8List key,
-    Uint8List salt,
-    int memory,
-    int iterations,
-    int length,
-    int parallelism,
-    int type,
-    int version,
-  ) {
-    final keyArray = Uint8Array.fromTypedList(key);
-//    final saltArray = Uint8Array.fromTypedList(salt);
-    final saltArray = allocate<Uint8>(count: salt.length);
-    final saltList = saltArray.asTypedList(length);
-    saltList.setAll(0, salt);
-//    const memoryCost = 1 << 16;
-
-//    _logger.fine('saltArray: ${ByteUtils.toHexList(saltArray.view)}');
-
-    final result = _argon2hash(
-      keyArray.rawPtr,
-      keyArray.length,
-      saltArray,
-      salt.length,
-      memory,
-      iterations,
-      parallelism,
-      length,
-      type,
-      version,
-    );
-
-    keyArray.free();
-//    saltArray.free();
-    free(saltArray);
-    final resultString = Utf8.fromUtf8(result);
-    return base64.decode(resultString);
-  }
+  Argon2Hash argon2hash;
 }
