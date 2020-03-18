7ec4be40325888f1eda783d46ecb5bf6813ccd0d
Herbert Poul <herbert@codeux.design>
2020-03-18 22:38:48 +0100

    sideload apk release

--- authpass/_tools/release.sh
+++ authpass/_tools/release.sh
@@ -72,6 +72,10 @@ case "$1" in
         export GRADLE_USER_HOME=$(pwd)/_tools/secrets/gradle_home
         $FLT build -v apk -t lib/env/production.dart --release --build-number $buildnumber --flavor samsungapps
     ;;
+    sideload)
+        export GRADLE_USER_HOME=$(pwd)/_tools/secrets/gradle_home
+        $FLT build -v apk -t lib/env/production.dart --release --build-number $buildnumber --flavor sideload
+    ;;
     playstoredev)
         export GRADLE_USER_HOME=$(pwd)/_tools/secrets/gradle_home
         $FLT build -v appbundle -t lib/env/production.dart --release --build-number $buildnumber --flavor playstoredev
