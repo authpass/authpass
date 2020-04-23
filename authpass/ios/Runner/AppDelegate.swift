ab38d2604d4aae42a7dfd4a3992f0bbc10810117
Herbert Poul <herbert@codeux.design>
2020-04-23 13:31:11 +0200

    ios: add nicer launchsplash screen, add AuthPass as handler/editor for kdbx.

--- authpass/ios/Runner/AppDelegate.swift
+++ authpass/ios/Runner/AppDelegate.swift
@@ -10,4 +10,14 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("Opening URL \(url) - options: \(options)")
+        // TODO we have to handle url opening.
+        if #available(iOS 9.0, *) {
+            let handled = super.application(app, open: url, options: options)
+            return handled
+        }
+        return false
+    }
 }
