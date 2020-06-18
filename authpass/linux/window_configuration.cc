a49b97ac27145c262c9535aef5a7f444ba7e4577
Herbert Poul <herbert@codeux.design>
2020-06-18 10:34:40 +0200

    linux: fix window title

--- authpass/linux/window_configuration.cc
+++ authpass/linux/window_configuration.cc
@@ -1,5 +1,5 @@
 #include "window_configuration.h"
 
-const char *kFlutterWindowTitle = "authpass";
+const char *kFlutterWindowTitle = "AuthPass";
 const unsigned int kFlutterWindowWidth = 1280;
 const unsigned int kFlutterWindowHeight = 720;
