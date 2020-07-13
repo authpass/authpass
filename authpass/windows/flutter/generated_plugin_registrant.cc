9f9cd184acacf1ce5e29a2bcbaae0a2aff62790b
Herbert Poul <herbert@codeux.design>
2020-07-13 07:57:44 +0200

    flutter create . for windows desktop

---
+++ authpass/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <file_chooser/file_chooser_plugin.h>
+#include <url_launcher_fde/url_launcher_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileChooserPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+}
