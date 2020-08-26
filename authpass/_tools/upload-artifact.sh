09a30505899fc5cb38fb357e31828df75a5448f6
Herbert Poul <herbert@codeux.design>
2020-08-26 10:07:00 +0200

    upload APKs also to data.authpass.app

--- authpass/_tools/upload-artifact.sh
+++ authpass/_tools/upload-artifact.sh
@@ -7,7 +7,7 @@ upload_file=$1
 curl --request POST \
     --url https://data.authpass.app/data/artifact.push \
     --fail \
-    --form token=$( cat authpass/_tools/secrets/artifact_token.txt ) \
-    --form upload=@authpass/${upload_file} \
-    --form filename=$( basename ${upload_file} )
+    --form token="$( cat authpass/_tools/secrets/artifact_token.txt )" \
+    --form upload="@${upload_file}" \
+    --form filename="$( basename "${upload_file}" )"
 
