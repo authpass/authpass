f8738eff82e8511d5325d1ad6dabaa562c49df47
Herbert Poul <herbert@codeux.design>
2020-09-02 16:05:18 +0200

    debug windows build

--- authpass/_tools/windows/create_release.sh
+++ authpass/_tools/windows/create_release.sh
@@ -32,6 +32,13 @@ VS_REDIST_FILES=(
 echo
 
 VS_REDIST=`find "${VS_REDIST_BASE}" -path '**0/x64/M**' -iname "${VS_REDIST_FILES[0]}" | grep -v onecore | head -n 1`
+
+if test -z "${VS_REDIST}" ; then
+  echo "UNABLE TO FIND VS REDIST (${VS_REDIST_FILES[0]})"
+  find "${VS_REDIST_BASE}"
+  exit 1
+fi
+
 VS_REDIST=`dirname "${VS_REDIST}"`
 
 echo "USING $VS_REDIST"
