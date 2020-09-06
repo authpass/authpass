0deaaa8a9cc8b0cc3b9572a0dcb92eca10ac02e5
Herbert Poul <herbert@codeux.design>
2020-09-06 12:24:09 +0200

    annotate files with string literals

--- authpass/_tools/run_analyse.sh
+++ authpass/_tools/run_analyse.sh
@@ -6,12 +6,22 @@ root="${0%/*}/.."
 
 pushd $root
 
+pushd ..
+reporoot="$(pwd)"
+popd
+
 _tools/flutter_run.sh pub global activate string_literal_finder
 _tools/flutter_run.sh pub global run \
   string_literal_finder \
   --path lib \
   --exclude-path l10n/ \
+  --annotations-output-file "${reporoot}/annotations.json" --annotations-path-root "${reporoot}" \
   --metrics-output-file metrics.json || true
+
+cat "${reporoot}/annotations.json" | jq -c '.[:50]' > "${reporoot}/annotations_first_50.json"
+total_annotations=$(cat "${reporoot}/annotations.json" | jq -c '. | length')
+
+echo "::set-output name=total_annotations::${total_annotations}"
 
 curl --request POST \
     --url https://data.authpass.app/data/artifact.push \
