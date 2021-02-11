c70ca64cdd94e0d010d4da7827e040e2b6145538
Herbert Poul <herbert@codeux.design>
2021-02-11 10:26:47 +0000

    upgrade to flutter 1.27

--- authpass/lib/generated_plugin_registrant.dart
+++ authpass/lib/generated_plugin_registrant.dart
@@ -5,15 +5,19 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:biometric_storage/biometric_storage_web.dart';
+import 'package:import_js_library/import_js_library.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   BiometricStoragePluginWeb.registerWith(registrar);
+  ImportJsLibrary.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
