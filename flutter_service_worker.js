87b583066bef6fee68afd21982bfbc3b6f7c3e18
hpoul <hpoul@users.noreply.github.com>
2021-02-20 11:41:42 +0000

    deploy: c87450331bdcbdbe610293ca3e7ff61b5ef15f76

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {
 "version.json": "049f3d93d88ff96aae80118eb3218b9e",
 "manifest.json": "a55e3e721de6d5444c9dc74af6ebb2c1",
 "favicon.png": "0f6bc457452337dff399ad2f7da1786a",
-"index.html": "107c2c472d2b291a184eaf73406ecaeb",
-"/": "107c2c472d2b291a184eaf73406ecaeb"
+"index.html": "dfd85c00456781863ab704ce345b753b",
+"/": "dfd85c00456781863ab704ce345b753b"
 };
 
 // The application shell files that are downloaded before a service worker can
