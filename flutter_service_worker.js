f13ec9a07c607903700de4b1ab56322cff926b21
hpoul <hpoul@users.noreply.github.com>
2021-08-13 11:11:54 +0000

    deploy: 11d08834304de2ad64abf4839fd7686c6039cd4f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,12 +32,12 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "00d68188a25b211f4d35c7c9535f084c",
 "assets/AssetManifest.json": "bd6d3296050772c529d4030ed8d3b7cb",
-"index.html": "6522fc8932bb757d0d284a87a795e15f",
-"/": "6522fc8932bb757d0d284a87a795e15f",
+"index.html": "97eb480d3d725c83f0b22cd30f45aea8",
+"/": "97eb480d3d725c83f0b22cd30f45aea8",
 "js/README.md": "20e28da2a4aaa5b4956d794075e1d6a6",
 "js/argon2-bundled.min.js": "6c006c4658c1954a2e78b68130f97ea7",
 "manifest.json": "a55e3e721de6d5444c9dc74af6ebb2c1",
-"main.dart.js": "178c2777e628e3ab86a1b9689a212e26",
+"main.dart.js": "56e0468c42a96d09b0be3c785eb6cf01",
 "version.json": "87642295d558cb11220adf87f4dfb17e",
 "favicon.png": "0f6bc457452337dff399ad2f7da1786a"
 };
