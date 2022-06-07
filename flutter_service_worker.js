08eb52db84412808fa0eb83d38265edc1423eaa6
hpoul <hpoul@users.noreply.github.com>
2022-06-07 13:37:14 +0000

    deploy: 8ac3c57300fef3021f91210067aaff41689d7a16

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "5a79ff2651d2a5f20b7414dd58138e45",
-"/": "5a79ff2651d2a5f20b7414dd58138e45",
+"index.html": "1664f86135a3527406d3d2cad8a95de5",
+"/": "1664f86135a3527406d3d2cad8a95de5",
 "favicon.ico": "5e9ecce5608cfbeda2db0b5e8be7fa7d",
 "manifest.json": "a55e3e721de6d5444c9dc74af6ebb2c1",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -40,7 +40,7 @@ const RESOURCES = {
 "version.json": "8f9631ff5c3fddd981fd9bbe5171e318",
 "js/README.md": "20e28da2a4aaa5b4956d794075e1d6a6",
 "js/argon2-bundled.min.js": "6c006c4658c1954a2e78b68130f97ea7",
-"main.dart.js": "17e0ef4d1b733dad9ad9b13f27dc4cb8",
+"main.dart.js": "c790b23c74688ef379316538af683c04",
 "icons/authpass-launcher-512.png": "b9ba6d8d7c5a550b37ed6937d5bea4de",
 "icons/authpass-launcher-192.png": "66bc9dfe1b283d730ec395348dc97d31"
 };
