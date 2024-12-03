e0d5a5d7feb5208807f809cea9a2ebf91d084e64
Herbert Poul <herbert@codeux.design>
2024-12-03 16:21:04 +0000

    update dependencies.

--- authpass/macos/Flutter/GeneratedPluginRegistrant.swift
+++ authpass/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,7 +19,7 @@ import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 import wakelock_plus
